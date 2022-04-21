f4c634c5a3bc0b569a6a32b437231149d0a85e5a
dzzirima <52695278+dzzirima@users.noreply.github.com>
2022-04-21 08:52:37 +0200

    feat :final code

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x5001E7F89952f7ae71e02DC960d8168a9912F6D9";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
